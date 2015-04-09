e44c73efd108aea4e579aa79f5882b1526ea283b
Chad <chadcodes@users.noreply.github.com>
2015-04-09 15:14:55 -0400

    git init

--- btDay3/main.m
+++ btDay3/main.m
@@ -4,14 +4,29 @@
 //
 //  Created by bitmaker on 2015-04-09.
 //  Copyright (c) 2015 chad. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 
+// Global function declaration
+float convertFahrenheightToCelsius(float);
+
+// Static function declaration
+// static float convertFahrenheightToCelsius(float);
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        float celsius = convertFahrenheightToCelsius(27.0);
+        NSLog(@"Your C is %.5f", celsius);
     }
     return 0;
+}
+
+// Function implementation
+// Function to convert Fahrenheit (F) to Celsius (C)
+float convertFahrenheightToCelsius(float f) {
+    // NSLog out input
+    // NSLog(@"Your F is %.2f", f);
+    float output = (f - 32) / 1.8;
+    return output;
 }
