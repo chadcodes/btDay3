0f648679af8bc80144830c6ad691482e59680aa9
Chad <chadcodes@users.noreply.github.com>
2015-04-09 14:24:35 -0400

    Initial Commit

---
+++ btDay3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  btDay3
+//
+//  Created by bitmaker on 2015-04-09.
+//  Copyright (c) 2015 chad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
