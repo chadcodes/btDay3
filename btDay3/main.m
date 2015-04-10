e7cb00c42cd01a14913146d676789b52cb57ec67
Chad <chadcodes@users.noreply.github.com>
2015-04-10 14:32:37 -0400

    day 3 done :)

--- btDay3/main.m
+++ btDay3/main.m
@@ -9,20 +9,27 @@
 
 // Global function declaration
 float convertFahrenheightToCelsius(float);
+int returnIntegers(int, int);
+int whichIsHigher(int, int);
+int startCounter(int, int);
 
 // Static function declaration
 // static float convertFahrenheightToCelsius(float);
 
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        float celsius = convertFahrenheightToCelsius(27.0);
-        NSLog(@"Your C is %.5f", celsius);
-    }
-    return 0;
+  @autoreleasepool {
+    float celsius = convertFahrenheightToCelsius(27.0);
+    int integerSum = returnIntegers(5, 5);
+    int integerHigher = whichIsHigher(8, 10);
+    NSLog(@"Your C is : %.5f", celsius);
+    NSLog(@"Your sum is : %d", integerSum);
+    NSLog(@"The highest integer is : %d", integerHigher);
+    startCounter(2, 12);
+  }
+  return 0;
 }
 
-// Function implementation
 // Function to convert Fahrenheit (F) to Celsius (C)
 float convertFahrenheightToCelsius(float f) {
     // NSLog out input
@@ -30,3 +37,37 @@ float convertFahrenheightToCelsius(float f) {
     float output = (f - 32) / 1.8;
     return output;
 }
+
+// Function to return Sum of integers
+int returnIntegers(int x, int y) {
+    int output = x + y;
+    return output;
+}
+
+// Function to return the highest Integer
+int whichIsHigher(int x, int y) {
+  // set my output var
+  int output = 0;
+  if (x > y) {
+    output = x;
+  } else if (y > x) {
+    output = y;
+  }
+  return output;
+}
+
+// Function to Loop and display the counter
+int startCounter(int min, int max) {
+  int counter = min;
+  while (counter < max) {
+    NSLog(@"The counter is at : %d",counter);
+    counter++;
+//    if (min == max) {
+//      break;
+//    }
+  }
+  return 0;
+}
+
+
+
